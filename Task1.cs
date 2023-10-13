331e660e93cb7608e5727e1f2aa6b228e5a6285f
Murad <145739695+Murgitadd@users.noreply.github.com>
2023-10-13 05:27:57 +0400

    Done Task1.cs

--- Task1.cs
+++ Task1.cs
@@ -1,10 +1,126 @@
-Console.WriteLine("Enter a four-digit number:");
+using System;
 
-int num = int.Parse(Console.ReadLine());
+namespace Task_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle[] vehicles = new Vehicle[2];
 
-int minlik = num / 1000;
-int yuzluk = (num % 1000) / 100;
-int onluq = (num % 100) / 10;
-int teklik = (num % 10);
+            Car myCar = new Car
+            {
+                FactoryName = "Toyota",
+                Model = "Camry",
+                Color = "Red",
+                DriveTime = 2,
+                DrivePath = 100,
+                DoorCount = 4,
+                IsElectricCar = false,
+            };
 
-Console.WriteLine(minlik+yuzluk+onluq+teklik);
+            Bicycle myBicycle = new Bicycle
+            {
+                FactoryName = "Schwinn",
+                Model = "Mountain Bike",
+                Color = "Blue",
+                DriveTime = 1,
+                DrivePath = 10,
+                Type = "Mountain Bike",
+            };
+
+            vehicles[0] = myCar;
+            vehicles[1] = myBicycle;
+
+            // Calculate and display the average speed for each vehicle
+            foreach (var vehicle in vehicles)
+            {
+            	vehicle.DefineNatureHarmness();
+                vehicle.AverageSpeed();
+                vehicle.GetInfo(); 
+                Console.WriteLine();
+            }
+
+            // TODO: Implement other functionality here
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+
+    abstract class Vehicle
+    {
+        public string FactoryName { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int DriveTime { get; set; }
+        public int DrivePath { get; set; }
+        private readonly DateTime ProductionDate;
+
+        public abstract void AverageSpeed();
+        public abstract void DefineNatureHarmness();
+        public override string ToString()
+        {
+            return FactoryName + " " + Model;
+        }
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("Factory Name: " + FactoryName);
+            Console.WriteLine("Model: " + Model);
+        }
+    }
+
+
+    class Car : Vehicle
+    {
+        public int DoorCount;
+        public bool IsElectricCar;
+
+         public override void DefineNatureHarmness()
+        {
+        	if (IsElectricCar)
+            {
+                Console.WriteLine("Low");
+            }
+            else
+            {
+                Console.WriteLine("High");
+            }
+        }
+        
+        public override void AverageSpeed()
+        {
+            int averageSpeed = DrivePath / DriveTime;
+            Console.WriteLine("Average Speed for Car: " + averageSpeed + " mph");
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine("Door Count: " + DoorCount);
+            Console.WriteLine("Is Electric Car: " + IsElectricCar);
+        }
+    }
+
+    class Bicycle : Vehicle
+    {
+        public string Type;
+
+        public override void AverageSpeed()
+        {
+            int averageSpeed = DrivePath / DriveTime;
+            Console.WriteLine("Average Speed for Bicycle: " + averageSpeed + " mph");
+        }
+        public override void DefineNatureHarmness()
+        {
+        	Console.WriteLine("None");        
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine("Type: " + Type);
+        }
+    }
+}
